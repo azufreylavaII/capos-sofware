9e6e412b2598110c6cf7130b74050188be06e566
azufreylavaII <cibertec986067353@gmail.com>
2024-08-15 02:34:17 +0000

    se cxreo n2 componetes

---
+++ musitec/src/components/Mensaje.jsx
@@ -0,0 +1,5 @@
+function Mensaje(){
+
+    return<h2> olioliioolik</h2>
+}
+ export default Mensaje;
---
+++ musitec/src/components/MiComponente.jsx
@@ -0,0 +1,6 @@
+function MiComponente(){
+
+
+    return <h1> Hola mundo</h1>
+}
+export default MiComponente;
